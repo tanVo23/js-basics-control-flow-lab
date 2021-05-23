1c74f39259dd4d33c1fad9437f14664be954a2c3
Tan <tan10tvo@gmail.com>
2021-05-23 17:38:45 -0600

    commit tree ebeebf613444cc4823734b59522e88863761b514

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
-}
+  if (distance <= 400){
+    return 'This one is on me!'
+    } else if (distance > 2000 && distance < 2500){
+      return 'I will gladly take your thirty bucks.'
+  } else if (distance > 2500){
+    return 'No can do.'
+  }
+} 
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC"){
+    return "Ok, sounds good."
+  }else {
+    return "No go."
+  }
 }
-
-function switchOnCharmFromTip(){
+//return (city === "NYC") ? "Ok, sounds good." : "No go."   -can also do it this way
+function switchOnCharmFromTip(tipString){
   // Write your code here!
+  switch(tipString){
+    case "generous":
+      return "Thank you so much."
+      case "not as generous":
+        return "Thank you."
+        case "thanks for everything":
+          return "Bye."
+  }
 }
